f2917ffe8b8bbc50c144fefcd7287b10d73b01b8
yangkun <1269769296@qq.com>
2020-06-20 11:22:32 +0800

    update shell and launch files

--- src/airsim_environment/shell/play.sh
+++ src/airsim_environment/shell/play.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-roslaunch airsim_environment rosbag_play.launch & PID0=$!
+Environment_names=("building" "neighborhood" "mountain" "soccer")
+path=$(cd `dirname $0`;pwd)
+
+if [ ! -n "$1" ]
+then
+    echo "Usage: ./play.sh Environment_name(${Environment_names[@]})"
+    exit 1
+fi
+
+if [[ ! "${Environment_names[@]}" =~ $1 ]]
+then
+    echo "Usage: ./play.sh Environment_name(${Environment_names[@]})"
+    exit 1
+fi
+
+roslaunch airsim_environment rosbag_play.launch folder:="$1" & PID0=$!
 
 wait
 kill PID0
--- src/airsim_environment/shell/plot.sh
+++ src/airsim_environment/shell/plot.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-rosrun plotjuggler PlotJuggler -d ~/guidance_ws/src/airsim_environment/bag/data.bag -l ~/guidance_ws/src/airsim_environment/juggler/plot.xml
+Environment_names=("building" "neighborhood" "mountain" "soccer")
+path=$(cd `dirname $0`;pwd)
+
+if [ ! -n "$1" ]
+then
+    echo "Usage: ./play.sh Environment_name(${Environment_names[@]})"
+    exit 1
+fi
+
+if [[ ! "${Environment_names[@]}" =~ $1 ]]
+then
+    echo "Usage: ./play.sh Environment_name(${Environment_names[@]})"
+    exit 1
+fi
+
+rosrun plotjuggler PlotJuggler -d ~/guidance_ws/src/airsim_environment/bag/$1/data.bag -l ~/guidance_ws/src/airsim_environment/juggler/plot.xml
